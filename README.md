28aaf898b1d4e5d70265b84c7ea347616fac5ca3
Portal <juanportalg@gmail.com>
2022-06-07 20:41:33 -0500

    GIF added and README updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Pokedex
+
+Still in progress...
+
+<img src="https://raw.githubusercontent.com/juanportal/Pokedex/main/cat.gif" width="200">
+
+This is a Pokedex created using React
+
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Developed by <a href="https://github.com/JuanPortal" target="_blank">Juan Portal</a>
+    </footer>
+  )
+}
+
+export default Footer;
