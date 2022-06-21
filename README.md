12fa8aa279c63a4efa64c21447af9cc0c7fa2d2c
Portal <juanportalg@gmail.com>
2022-06-21 01:09:42 -0500

    from scratch using vite

--- README.md
+++
@@ -1,8 +0,0 @@
-# Pokedex
-
-Still in progress...
-
-<img src="https://raw.githubusercontent.com/juanportal/Pokedex/main/public/media/progress.gif" width="200">
-
-This is a Pokédex created using React and the Pokémon API
-
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-
-const Footer = () => {
+export const Footer = () => {
   return (
     <footer>
         Developed by <a href="https://github.com/JuanPortal" target="_blank">Juan Portal</a>
     </footer>
   )
 }
-
-export default Footer;
